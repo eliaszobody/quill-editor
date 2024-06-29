32f48ca4a0d2c2d1565be0db219d1db66a7c309e
eliaszobody <elias@szobody.com>
2024-06-29 10:26:38 -0400

    testing

--- src/QuillEditor.php
+++ src/QuillEditor.php
@@ -6,6 +6,6 @@ use Filament\Forms\Components\Field;
 
 class QuillEditor extends Field
 {
-    protected string $view = 'quill-editor';
+    protected string $view = 'quill-editor::quill-editor';
 }
 
